e3dad2d27e57ce92bfb2ea50bcf93db841789322
DanielMakokha <danielmakokhan@gmail.com>
2023-07-21 13:17:38 +0300

    js p2

--- script.js
+++ script.js
@@ -4,4 +4,7 @@ document.body.addEventListener('mouseover',(e)=>{
     var x=((window.innerWidth / 3) - e.pageX) / 20;
     var y=((window.innerHeight / 3) - e.pageY) /20;
     container.style.transform = `rotateX(${-y*2}deg) rotateY(${-x*2}deg) translateY(${x*2}px) translateX(${y*2}px)`;
+    person.forEach(person =>{
+        person.style.transform=`translateY(${y*2}px) translateX(${x*2}px)`
+    })
 })
