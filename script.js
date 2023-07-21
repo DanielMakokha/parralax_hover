36c3624ba0cc4c711c268feff0b2fb3697f8f21f
DanielMakokha <danielmakokhan@gmail.com>
2023-07-21 13:17:16 +0300

    js 1

---
+++ script.js
@@ -0,0 +1,7 @@
+const container=document.querySelector('.container');
+const person=document.querySelectorAll('.person');
+document.body.addEventListener('mouseover',(e)=>{
+    var x=((window.innerWidth / 3) - e.pageX) / 20;
+    var y=((window.innerHeight / 3) - e.pageY) /20;
+    container.style.transform = `rotateX(${-y*2}deg) rotateY(${-x*2}deg) translateY(${x*2}px) translateX(${y*2}px)`;
+})
